7c3947cb3a865e2739e0fc3ce30189f2b1a5e759
Alice <am171696@alunos.unisanta.br>
2021-01-06 13:52:43 -0300

    Delegando salvamento do cargo no banco de dados para a CrudCargoService

--- src/main/java/br/com/alura/spring/data/SpringDataApplication.java
+++ src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -1,19 +1,24 @@
 package br.com.alura.spring.data;
 
+import java.util.Scanner;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.alura.spring.data.orm.Cargo;
-import br.com.alura.spring.data.repository.CargoRepository;
+import br.com.alura.spring.data.service.CrudCargoService;
 
 @SpringBootApplication
 public class SpringDataApplication implements CommandLineRunner{
 	
-	private final CargoRepository repository;
+	private final CrudCargoService cargoService;
+	private boolean system;
+
 	
-	public SpringDataApplication(CargoRepository repository) {
-		this.repository = repository;
+	public SpringDataApplication(CrudCargoService cargoService) {
+		this.cargoService = cargoService;
+		this.system = true;
 	}
 
 	public static void main(String[] args) {
@@ -22,9 +27,23 @@ public class SpringDataApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		Cargo desenvolvedor = new Cargo();
-		desenvolvedor.setDescricao("DESENVOLVEDOR DE SOFTWARE");
-		repository.save(desenvolvedor);
+		Scanner scanner = new Scanner(System.in);
+		int acao;
+		
+		while(system) {
+			System.out.println("0 -> Sair da aplicação");
+			System.out.println("1 -> Cargo");
+			System.out.print("Escolha uma ação: ");
+
+			acao = scanner.nextInt();
+			
+			if(acao == 1) {
+				cargoService.inicial(scanner);
+			} else {
+				system = false;
+			}
+			
+		}
 		
 	}
 
