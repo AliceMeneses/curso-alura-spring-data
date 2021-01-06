9b1873724dd35562f6d049fb8fe0c38cda819b16
Alice <am171696@alunos.unisanta.br>
2021-01-06 17:19:20 -0300

    Delegando CRUD para as services

--- src/main/java/br/com/alura/spring/data/SpringDataApplication.java
+++ src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -6,19 +6,22 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.alura.spring.data.orm.Cargo;
 import br.com.alura.spring.data.service.CrudCargoService;
+import br.com.alura.spring.data.service.CrudFuncionarioService;
+import br.com.alura.spring.data.service.CrudUnidadeTrabalhoService;
 
 @SpringBootApplication
-public class SpringDataApplication implements CommandLineRunner{
-	
-	private final CrudCargoService cargoService;
-	private boolean system;
+public class SpringDataApplication implements CommandLineRunner {
 
-	
-	public SpringDataApplication(CrudCargoService cargoService) {
+	private final CrudCargoService cargoService;
+	private final CrudFuncionarioService funcionarioService;
+	private final CrudUnidadeTrabalhoService unidadeTrabalhoService;
+
+
+	public SpringDataApplication(CrudCargoService cargoService, CrudFuncionarioService funcionarioService, CrudUnidadeTrabalhoService unidadeTrabalhoService) {
 		this.cargoService = cargoService;
-		this.system = true;
+		this.funcionarioService = funcionarioService;
+		this.unidadeTrabalhoService = unidadeTrabalhoService;
 	}
 
 	public static void main(String[] args) {
@@ -27,24 +30,37 @@ public class SpringDataApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
+		boolean system = true;
 		Scanner scanner = new Scanner(System.in);
-		int acao;
-		
-		while(system) {
+		int action;
+
+		while (system) {
 			System.out.println("0 -> Sair da aplicação");
 			System.out.println("1 -> Cargo");
+			System.out.println("2 -> Funcionário");
+			System.out.println("3 -> Unidade de Trabalho");
+
 			System.out.print("Escolha uma ação: ");
 
-			acao = scanner.nextInt();
-			
-			if(acao == 1) {
+			action = scanner.nextInt();
+
+			switch (action) {
+			case 1:
 				cargoService.inicial(scanner);
-			} else {
+				break;
+			case 2:
+				funcionarioService.inicial(scanner);
+				break;
+			case 3:
+				unidadeTrabalhoService.inicial(scanner);
+				break;
+			default:
 				system = false;
+				break;
 			}
-			
+
 		}
-		
+
 	}
 
 }
