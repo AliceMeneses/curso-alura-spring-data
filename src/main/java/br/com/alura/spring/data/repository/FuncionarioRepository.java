48c2bf56c1f7f0c7e8baf8eb0a5c05fd523b1364
Alice <alicesilvademeneses@gmail.com>
2021-01-07 17:28:17 -0300

    Adicionando query findNomeDataContratacaoSalarioMaior(...) com JPQL

--- src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
+++ src/main/java/br/com/alura/spring/data/repository/FuncionarioRepository.java
@@ -1,7 +1,10 @@
 package br.com.alura.spring.data.repository;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import br.com.alura.spring.data.orm.Funcionario;
@@ -9,5 +12,8 @@ import br.com.alura.spring.data.orm.Funcionario;
 public interface FuncionarioRepository extends CrudRepository<Funcionario, Integer>{
 	
 	List<Funcionario> findByNome(String nome);
+	
+	@Query("Select f Funcionario f where f.nome = :pNome and f.data = :pData f.salario > :pSalario")
+	List<Funcionario> findNomeDataContratacaoSalarioMaior(String pNome, LocalDate pData, BigDecimal pSalario);
 
 }
